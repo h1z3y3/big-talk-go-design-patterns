c711f2c696a0993dde13e132dc1155b442302692
hezhaoyang <gmzhaoyang@gmail.com>
2018-11-25 16:57:55 +0800

    add annonation

--- 03_proxy/proxy.go
+++ 03_proxy/proxy.go
@@ -4,16 +4,19 @@ type Image interface {
 	Get() string
 }
 
+// 真实的图片类
 type RealImage struct {}
 
 func (r *RealImage) Get() string {
 	return "real_image_url"
 }
 
+// 代理类
 type ImageProxy struct {
 	realImage RealImage
 }
 
+// 由代理类进行原类的调用，从而能在原类基础上做一些操作
 func (r *ImageProxy) Get() string {
 
 	var res string
