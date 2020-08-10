163bc09504056abdd8f2ffecc78d93113007a5f1
hezhaoyang <gmzhaoyang@gmail.com>
2020-08-10 20:20:58 +0800

    feat: add builder pattern

--- 06_builder/builder.go
+++ 06_builder/builder.go
@@ -15,7 +15,29 @@ type Car struct {
 }
 
 func (c *Car) Description() string {
-	return fmt.Sprintf("Car: %s, %s", c.Color, c.Number)
+	return fmt.Sprintf("Car: color: %s, number: %s", c.Color, c.Number)
+}
+
+// Builder interface
+type Builder interface {
+	SetColor(string) Builder
+	SetNumber(string) Builder
+	Build() (IProduct, error)
+}
+
+// Director
+type Director struct {
+	builder Builder
+}
+
+func NewDirector(b Builder) *Director {
+	return &Director{
+		builder: b,
+	}
+}
+
+func (d *Director) Generate() (IProduct, error) {
+	return d.builder.SetColor("red").SetNumber("12345").Build()
 }
 
 // Concrete Builder
@@ -27,13 +49,13 @@ func NewCarBuilder() *CarBuilder {
 	return &CarBuilder{}
 }
 
-func (c *CarBuilder) SetColor(a string) *CarBuilder {
+func (c *CarBuilder) SetColor(a string) Builder {
 	c.car.Color = a
 
 	return c
 }
 
-func (c *CarBuilder) SetNumber(b string) *CarBuilder {
+func (c *CarBuilder) SetNumber(b string) Builder {
 	c.car.Number = b
 	return c
 }
--- 06_builder/builder_test.go
+++ 06_builder/builder_test.go
@@ -7,14 +7,13 @@ import (
 
 func TestNewAdd01(t *testing.T) {
 
-	builder := NewCarBuilder()
-
-	car, err := builder.SetColor("red").SetNumber("123455").Build()
+	director := NewDirector(&CarBuilder{})
+	adCar, err := director.Generate()
 
 	if err != nil {
 		t.Fatal("error:", err)
 	}
 
-	fmt.Println("car: ", car.Description())
+	fmt.Println(adCar.Description())
 
 }
